85b244cd5801dfc0931d61dd250f4e8abd622a8e
alex <ssstik4@gmail.com>
2019-10-31 14:52:44 +0300

    make query support

--- index.py
+++ index.py
@@ -10,17 +10,16 @@ class InvertedIndex:
         self.stemmer = SnowballStemmer("russian")
 
     # (count of occurrences, dict_of_documents)
-    def add(self, not_stemmed_word, message_id, channel):
-        stemmed_word = self.stemmer.stem(not_stemmed_word)
+    def add(self, stemmed_word, message_id, channel):
         if stemmed_word in self.index:
-            item = self.index[stemmed_word]
+            item = self.index[stemmed_word][0]
+            self.index[stemmed_word][1] += 1
             if (message_id, channel) in item:
                 item[(message_id, channel)] += 1
             else:
                 item[(message_id, channel)] = 1
         else:
-            self.index[stemmed_word] = {(message_id, channel): 1}
-
+            self.index[stemmed_word] = [{(message_id, channel): 1}, 1]
 
     def replace_punctuation(sentence: str):
         res = sentence
@@ -28,10 +27,10 @@ class InvertedIndex:
             res = res.replace(punc, " ")
         return res
 
-    def process_sentence(sentence : str):
+    def process_sentence(self, sentence: str):
         processed_sentence = InvertedIndex.replace_punctuation(sentence)
         words = processed_sentence.split(" ")
-        words = [word for word in words if word not in russian_stop_words]
+        words = [self.stemmer.stem(word) for word in words if word not in russian_stop_words]
         return words
 
     # rows = [[sentence, message_id, channel]]
@@ -40,20 +39,39 @@ class InvertedIndex:
             sentence = row[0]
             message_id = row[1]
             channel = row[2]
-            words = InvertedIndex.process_sentence(sentence)
+            words = self.process_sentence(sentence)
             for word in words:
                 self.add(
-                    not_stemmed_word=word,
+                    stemmed_word=word,
                     message_id=message_id,
                     channel=channel
                 )
 
+    def search_phrase(self, query, limit=3):
+        query_words = self.process_sentence(query)
+        result = {}
+        for word in query_words:
+            if word in self.index:
+                messages = self.index[word][0]
+                count = self.index[word][1]
+                for message in messages.keys():
+                    if message in result:
+                        result[message] += messages[message] / float(count)
+                    else:
+                        result[message] = messages[message] / float(count)
+        result = sorted(result.items(), key=lambda x: x[1], reverse=True)[:limit]
+        return result
+
+
 # index = InvertedIndex()
-# rows=[
-#     ["Григорий пошёл домой на селиваново!","1","первый"],
-#     ["Григорий пошёл домой на селиваново!","1","второй"],
-#     ["Иванов пошёл домой на селиваново!","3","второй"],
+# rows = [
+#     ["Григорий пошёл домой на селиваново!", "1", "канал1"],
+#     ["Григорий пошёл домой на селиваново!", "1", "канал2"],
+#     ["Иванов пошёл домой на селиваново!", "2", "канал2"],
+#     ["Дрозды летели низко, но это хорошо!", "4", "канал1"],
 # ]
 # index.create_index(rows)
+#
+# print(index.search_phrase("Дрозды пошёл"))
 #
 # print(index.index)
